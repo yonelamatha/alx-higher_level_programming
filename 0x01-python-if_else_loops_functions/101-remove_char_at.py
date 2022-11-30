90a4df7c894f7b57111c8edc9452b077ed505cdd
yonelamatha <yonelamatha@gmail.com>
2022-11-29 19:46:06 -0800

    task15

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def magic_calculation(a, b, c):
-    if a < b:
-        return c
-    elif c > b:
-        return a + b
-    return a * b - c
+# 101-remove_char_at.py
+def remove_char_at(str, n):
+    """Create a copy of the string without the character at position n."""
+    if n < 0:
+        return (str)
+    return (str[:n] + str[n+1:])
