d4042ac15d859721175c4fba24ede973212c93c0
yonelamatha <yonelamatha@gmail.com>
2022-12-04 05:49:00 -0800

    task6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         if len(row) == 0:
-            print()
+           print()
         for n in range(len(row)):
             print('{:d}'.format(row[n]),
-                 end='\n' if n == len(row) - 1 else ' ')
+                  end='\n' if n == len(row) - 1 else ' ')
