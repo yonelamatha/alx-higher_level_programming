91315dc7109282f873a1cbf55cee983f6fc0ef76
yonelamatha <yonelamatha@gmail.com>
2023-01-05 01:20:45 -0800

    task

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,4 +1,6 @@
- backtracking program to print the coordinates of n queens
+#!/usr/bin/python3
+"""
+nqueens backtracking program to print the coordinates of n queens
 on an nxn grid such that they are all in non-attacking positions
 """
 
