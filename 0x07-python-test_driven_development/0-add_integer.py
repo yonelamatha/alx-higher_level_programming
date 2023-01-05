28bfa7a8f742de2dec0bb6229eee0b8a21bf0bd4
yonelamatha <yonelamatha@gmail.com>
2023-01-05 01:53:44 -0800

    task

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
 """
-add_integer:
-    Checks if parameters are int
-    Returns sum of parameters
+This module is composed by a function that adds two numbers
 """
 
-def add_integer(a, b=98):
-    """
-    Checks if int, otherwise return sum
-    """
-    if type(a) == float or type(b) == float:
-        a = int(a)
-        b = in
 
-    if type(a) != int:
+def add_integer(a, b=98):
+    """ Function that adds two integer and/or float numbers
+    Args:
+        a: first number
+        b: second number
+    Returns:
+        The addition of the two given numbers
+    Raises:
+        TypeError: If a or b aren't integer and/or float numbers
+    """
+
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    elif type(b) != int:
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
-    else:
-        return a + b
+    a = int(a)
+    b = int(b)
+    return (a + b)
