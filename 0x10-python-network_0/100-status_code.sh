d5bd9c4030c04e254f7d7c14c39ae0d4d22f47b9
yonelamatha <yonelamatha@gmail.com>
2023-03-30 09:08:14 -0700

    task100

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-# sends a request to a URL passed as an argument, and displays only the status code of the response
-curl -sI -w '%{response_code}' "$1" -o /dev/null
+#!/bin/bash 
+# script that sends a request to a URL passed as an argument, and displays only the status code of the response.
+curl -s -L -X HEAD -w "%{http_code}" "$1"
