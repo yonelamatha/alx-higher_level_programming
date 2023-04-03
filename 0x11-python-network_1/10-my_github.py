1a4d40e8a3b2aab72b2bc06facc27652b049c643
yonelamatha <yonelamatha@gmail.com>
2023-04-02 21:38:44 -0700

    task9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""  fetches github user repo commits  """
+"""script that takes your GitHub credentials (username and password) and
+uses the GitHub API to display your id.
+The first argument will be your username.
+The second argument will be your password (in your case, a personal
+access token as password).
+"""
+import sys
 import requests
-from sys import argv
+from requests.auth import HTTPBasicAuth
+
 
 if __name__ == "__main__":
-    r = requests.get('https://api.github.com/repos/{}/{}/commits'
-                     .format(argv[2], argv[1]))
-    counter = 0
-    for commit in sorted(r.json(), key=lambda c: c.get('commit')
-                         .get('author').get('date'), reverse=True):
-        print(commit.get('sha') + ": ", end="")
-        print(commit.get('commit').get('author').get('name'))
-        counter += 1
-        if counter == 10:
-            break
+    url = "https://api.github.com/user"
+    username = sys.argv[1]
+    password = sys.argv[2]
+    response = requests.get(url, auth=HTTPBasicAuth(username, password))
+    json_obj = response.json()
+    print(json_obj.get("id"))
