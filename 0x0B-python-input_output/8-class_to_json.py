6f47e0e3ef55ffcb1fa752096ba97a2b5ab20d1e
yonelamatha <yonelamatha@gmail.com>
2023-04-04 12:11:11 -0700

    task9

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-""" My class module
-"""
+""" Class to JSON """
 
 
-class MyClass:
-    """ My class
-    """
-
-    def __init__(self, name):
-        self.name = name
-        self.number = 0
-
-    def __str__(self):
-        return "[MyClass] {} - {:d}".format(self.name, self.number)
+def class_to_json(obj):
+    """ Returns the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object"""
+    return (obj.__dict__)
