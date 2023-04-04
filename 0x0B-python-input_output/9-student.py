6c49181eca286d365a3a634d1bb78640e2dba189
yonelamatha <yonelamatha@gmail.com>
2023-04-04 12:08:51 -0700

    task9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,30 +1,15 @@
 #!/usr/bin/python3
-"""Module defining the class Student"""
+""" Student to JSON """
 
 
 class Student:
-    """
-    Class that defines properties of student.
-    Attributes:
-        first_name (str): first name of student.
-        last_name (int): last name of student.
-        age (int): age of student.
-    """
+    """ New class student """
+
     def __init__(self, first_name, last_name, age):
-        """Creates new instances of Student.
-        Args:
-            first_name (str): first name of student.
-            last_name (int): last name of student.
-            age (int): age of student.
-        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Retrieves a dictionary representation of a Student instance.
-        Returns:
-            dict: dictionary representation.
-        """
-        return self.__dict__
+        return (self.__dict__)
 
