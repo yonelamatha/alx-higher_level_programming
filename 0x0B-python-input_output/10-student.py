b36ab04a44747161bdcac24e62660110414455a3
yonelamatha <yonelamatha@gmail.com>
2023-04-04 12:15:30 -0700

    task10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,41 +1,21 @@
 #!/usr/bin/python3
-"""Module defining the class Student based on 9-student.py"""
+""" Student to JSON """
 
 
 class Student:
-    """
-    Class that defines properties of student.
-    Attributes:
-        first_name (str): first name of student.
-        last_name (int): last name of student.
-        age (int): age of student.
-    """
+    """ New class student """
+
     def __init__(self, first_name, last_name, age):
-        """Creates new instances of Student.
-        Args:
-            first_name (str): first name of student.
-            last_name (int): last name of student.
-            age (int): age of student.
-        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieves a dictionary representation of a Student instance.
-        If attrs is a list of strings, only attribute names contained in,
-        this list must be retrieved.
-        Otherwise, all attributes must be retrieved.
-        Returns:
-            dict: dictionary representation.
-        """
         if attrs is None:
-            return self.__dict__
-
-        new_dict = {}
-        for item in attrs:
-            try:
-                new_dict[item] = self.__dict__[item]
-            except Exception:
-                pass
-        return 
+            return (self.__dict__)
+        else:
+            dic = {}
+            for nm in attrs:
+                if hasattr(self, nm):
+                    dic[nm] = getattr(self, nm)
+            return (dic)
